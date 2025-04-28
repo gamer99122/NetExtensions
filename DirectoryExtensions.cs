12af3876bfd3f2a8b695ea9c17cf665c31970e25
gamer99122 <gamer99122@hotmail.com.tw>
2025-04-28 10:27:33 +0800

    Add DirectoryExtensions

---
+++ DirectoryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtensions
+{
+    public static class DirectoryExtensions
+    {
+        /// <summary>
+        /// 確保指定的資料夾存在，如果不存在則創建它。
+        /// </summary>
+        /// <param name="path">資料夾目錄</param>
+        public static void pDirectoryEnsureExists(this string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
